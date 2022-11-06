8fa5f398c056d414ec5b6c2c8b95674cab3a7ab7
Petar Ivanov <petarivanov95@gmail.com>
2022-11-06 20:32:34 +0200

    More_on_dictionaries

---
+++ Dictionaries/Exercises/company_users.py
@@ -0,0 +1,19 @@
+companies = {}
+
+while True:
+    user_input =  input()
+    if user_input == 'End':
+        break
+    company_name, company_id = user_input.split(' -> ')
+    if company_name not in companies:
+        companies[company_name] = [company_id]
+    else:
+        if company_id in companies[company_name]:
+            pass
+        else:
+            companies[company_name].append(company_id)
+
+for company,id in companies.items():
+    print(company)
+    for user in companies[company]:
+        print(f'-- {user}')
---
+++ Dictionaries/Exercises/student_academy.py
@@ -0,0 +1,18 @@
+rows = int(input())
+students = {}
+
+def average(lst):
+    return sum(lst) / len(lst)
+
+for info in range(rows):
+    student = input()
+    grade = float(input())
+    if student in students:
+        students[student].append(grade)
+    else:
+        students[student] = [grade]
+    
+final_directory = {x:average(y) for x,y in students.items() if average(y) >= 4.50}
+
+for name,averageGrade in final_directory.items():
+    print(f'{name} -> {averageGrade:.2f}')
