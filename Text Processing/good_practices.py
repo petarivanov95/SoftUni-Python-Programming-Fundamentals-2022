edf636f22baa35664a961ff48e89414ce1fef3ca
Petar Ivanov <petarivanov95@gmail.com>
2022-11-10 22:33:36 +0200

    some_problems

---
+++ Text Processing/good_practices.py
@@ -0,0 +1,9 @@
+info = ['a','b','c','d','e','f']
+
+for x in range(len(info)):
+    print(info[x])
+
+print(10*'*')
+
+for idx, x in enumerate(info):
+    print(x, idx)
---
+++ The Big Book of Small Problems/bagels.py
@@ -0,0 +1,7 @@
+import random
+
+digit = random.choice(range(100,999))
+
+# first_guess = input()
+
+print(digit)
