def223262f23b105ce93b4215be05545cf9bd659
Petar Ivanov <petarivanov95@gmail.com>
2023-03-11 09:02:43 +0200

    in_progress

---
+++ 28. Text Processing - Exercise/12_treasure_finder.py
@@ -0,0 +1,20 @@
+keys = input().split(' ')
+command = input()
+
+while True:
+    if command == 'find':
+        break
+    else:
+        command = input().split(' ')
+        print(command)
+        
+    command = input()
+        
+
+
+# my_list = [1,2,3,4]
+# times = 16
+# for x in range(times):
+
+#     print(my_list[x % len(my_list)])
+
