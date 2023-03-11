5f7cf0902bf5446186672edd3cf01675528068b9
Petar Ivanov <petarivanov95@gmail.com>
2023-03-11 09:34:46 +0200

    treasure_finder solution

--- 28. Text Processing - Exercise/12_treasure_finder.py
+++ 28. Text Processing - Exercise/12_treasure_finder.py
@@ -1,20 +1,47 @@
+# print(ord('A')) 
+# print(chr(73))
 keys = input().split(' ')
-command = input()
+strings = []
 
 while True:
+    command = input()
     if command == 'find':
         break
-    else:
-        command = input().split(' ')
-        print(command)
-        
-    command = input()
-        
+    messcoords = command
+    strings.append(messcoords)
+
+decripted_strings = []
+
+for line in strings:
+    new_str = ''
+    for id, char in enumerate(line):
+        new_str += chr(ord(char) - int(keys[id % len(keys)]))
+
+    decripted_strings.append(new_str)
 
 
-# my_list = [1,2,3,4]
-# times = 16
-# for x in range(times):
+for line in decripted_strings:
+    type_ids = []
+    coords_start = 0
+    coords_end = 0
+    for id, char in enumerate(line):
+        if char == '&':
+            type_ids.append(int(id))
+        elif char == '<':
+            coords_start = int(id)
+        elif char == '>':
+            coords_end = int(id)
 
-#     print(my_list[x % len(my_list)])
+
+    the_type = line[type_ids[0] + 1 : type_ids[1]]
+    coords = line[coords_start + 1 : coords_end]
+    print(f"Found {the_type} at {coords}")
+    
+
+
+# keys = [1,2,1,3]
+
+# for x in range(20):
+
+#     print(keys[x % len(keys)])
 
