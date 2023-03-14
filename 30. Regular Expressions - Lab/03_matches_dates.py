c8790eec9ded3a538dfbc181b354f0931105bc04
Petar Ivanov <petarivanov95@gmail.com>
2023-03-14 22:12:49 +0200

    03_update

---
+++ 30. Regular Expressions - Lab/03_matches_dates.py
@@ -0,0 +1,10 @@
+import re
+
+# pattern = re.compile(r"(\d{2})([\/\-\.])([A-Z][a-z]{2})\2(\d{4})")
+pattern = re.compile(r"(\d{2})(.|-|\/)([A-Z][a-z]{2})\2(\d{4})")
+text = input()
+
+results = pattern.finditer(text)
+
+for result in results:
+    print(f"Day: {result.group(1)}, Month: {result.group(3)}, Year: {result.group(4)}")
